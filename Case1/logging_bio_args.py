ab8993f0cea0f53ce3824511429b5e571f186054
awerner <awerner@bfg.robohub.eng.uwaterloo.ca>
2024-10-24 14:42:03 +0000

    uss SubprocVecEnv, use PPO be default

--- Case1/logging_bio_args.py
+++ Case1/logging_bio_args.py
@@ -57,153 +57,169 @@ def plot_results(log_folder, title="Learning Curve"):
     plt.close()
 
 
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+    parser = argparse.ArgumentParser()
 
-########### training and data info ###########
-parser.add_argument(
-    "--total_timesteps", type=float, default=1e6,
-)
+    ########### training and data info ###########
+    parser.add_argument(
+        "--total_timesteps", type=float, default=1e6,
+    )
 
-parser.add_argument(
-    "--SEED", type=int, default=0,
-)
+    parser.add_argument(
+        "--SEED", type=int, default=0,
+    )
 
-parser.add_argument(
-    "--timesteps_per_batch", type=int, default=8000,
-)
+    parser.add_argument(
+        "--timesteps_per_batch", type=int, default=8000,
+    )
 
-parser.add_argument(
-    "--algo_name", type=str, default="TRPO",
-)
+    parser.add_argument(
+        "--algo_name", type=str, default="TRPO",
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.algo_name == "TRPO":
-    MLP = MlpPolicy
-    algo = TRPO
-    batchsize = "timesteps_per_batch"
-    offpolicy = False
-elif args.algo_name == "PPO":
-    MLP = MlpPolicy
-    algo = PPO1
-    batchsize = "timesteps_per_actorbatch"
-    offpolicy = False
-elif args.algo_name == "DDPG":
-    MLP = MlpPolicy_DDPG
-    algo = DDPG
-    batchsize = "nb_rollout_steps"
-    offpolicy = True
-elif args.algo_name == "TD3":
-    MLP = MlpPolicy_TD3
-    algo = TD3
-    batchsize = "train_freq"
-    offpolicy = True
-elif args.algo_name == "SAC":
-    MLP = MlpPolicy_SAC
-    algo = SAC
-    batchsize = "train_freq"
-    offpolicy = True
+    if args.algo_name == "TRPO":
+        MLP = "MlpPolicy"
+        algo = TRPO
+        batchsize = "timesteps_per_batch"
+        offpolicy = False
+    elif args.algo_name == "PPO":
+        MLP = "MlpPolicy"
+        algo = PPO
+        batchsize = "n_steps"
+        offpolicy = False
+    elif args.algo_name == "DDPG":
+        MLP = "MlpPolicy"
+        algo = DDPG
+        batchsize = "nb_rollout_steps"
+        offpolicy = True
+    elif args.algo_name == "TD3":
+        MLP = "MlpPolicy"
+        algo = TD3
+        batchsize = "train_freq"
+        offpolicy = True
+    elif args.algo_name == "SAC":
+        MLP = "MlpPolicy"
+        algo = SAC
+        batchsize = "train_freq"
+        offpolicy = True
 
-# Mode 4 corresponds to randomly moving target
-args.mode = 4
+    # Mode 4 corresponds to randomly moving target
+    args.mode = 4
 
-# Set simulation final time
-final_time = 10
-# Number of control points
-number_of_control_points = 6
-# target position
-target_position = [-0.4, 0.6, 0.2]
-# learning step skip
-num_steps_per_update = 7
-# alpha and beta spline scaling factors in normal/binormal and tangent directions respectively
-args.alpha = 75
-args.beta = 75
+    # Set simulation final time
+    final_time = 10
+    # Number of control points
+    number_of_control_points = 6
+    # target position
+    target_position = [-0.4, 0.6, 0.2]
+    # learning step skip
+    num_steps_per_update = 7
+    # alpha and beta spline scaling factors in normal/binormal and tangent directions respectively
+    args.alpha = 75
+    args.beta = 75
 
-sim_dt = 2.0e-4
+    sim_dt = 2.0e-4
 
-max_rate_of_change_of_activation = np.infty
-print("rate of change", max_rate_of_change_of_activation)
+    max_rate_of_change_of_activation = np.infty
+    print("rate of change", max_rate_of_change_of_activation)
 
-# If True, train. Otherwise run trained policy
-args.TRAIN = True
+    # If True, train. Otherwise run trained policy
+    args.TRAIN = True
 
-env = Environment(
-    final_time=final_time,
-    num_steps_per_update=num_steps_per_update,
-    number_of_control_points=number_of_control_points,
-    alpha=args.alpha,
-    beta=args.beta,
-    COLLECT_DATA_FOR_POSTPROCESSING=not args.TRAIN,
-    mode=args.mode,
-    target_position=target_position,
-    target_v=0.5,
-    boundary=[-0.6, 0.6, 0.3, 0.9, -0.6, 0.6],
-    E=1e7,
-    sim_dt=sim_dt,
-    n_elem=20,
-    NU=30,
-    num_obstacles=0,
-    dim=3.0,
-    max_rate_of_change_of_activation=max_rate_of_change_of_activation,
-)
+    from typing import Callable
 
-name = str(args.algo_name) + "_3d-tracking_id"
-identifer = name + "-" + str(args.timesteps_per_batch) + "_" + str(args.SEED)
+    def make_env(rank: int, seed: int = 0) -> Callable:
+        def _init() -> gym.Env:
+            env = Environment(
+                final_time=final_time,
+                num_steps_per_update=num_steps_per_update,
+                number_of_control_points=number_of_control_points,
+                alpha=args.alpha,
+                beta=args.beta,
+                COLLECT_DATA_FOR_POSTPROCESSING=not args.TRAIN,
+                mode=args.mode,
+                target_position=target_position,
+                target_v=0.5,
+                boundary=[-0.6, 0.6, 0.3, 0.9, -0.6, 0.6],
+                E=1e7,
+                sim_dt=sim_dt,
+                n_elem=20,
+                NU=30,
+                num_obstacles=0,
+                dim=3.0,
+                max_rate_of_change_of_activation=max_rate_of_change_of_activation,
+            )
+            env = Monitor(env)
+            env.reset(seed=seed+rank) # stupid
+            return env
+        return _init
+
+    num_cpu = 92  # Number of processes to use
+    vec_env = SubprocVecEnv([make_env(args.SEED*1000+i) for i in range(num_cpu)])
 
 
-if args.TRAIN:
-    log_dir = "./log_" + identifer + "/"
-    os.makedirs(log_dir, exist_ok=True)
-    env = Monitor(env, log_dir)
+    name = str(args.algo_name) + "_3d-tracking_id"
+    identifer = name + "-" + str(args.timesteps_per_batch) + "_" + str(args.SEED)
 
 
-from stable_baselines.results_plotter import ts2xy, plot_results
-from stable_baselines import results_plotter
+    if args.TRAIN:
+        log_dir = "./log_" + identifer + "/"
+        os.makedirs(log_dir, exist_ok=True)
+        #env = Monitor(vec_env, log_dir)
 
-if args.TRAIN:
-    if offpolicy:
-        if args.algo_name == "TD3":
+
+
+    if args.TRAIN:
+        if offpolicy:
+            if args.algo_name == "TD3":
+                items = {
+                    "policy": MLP,
+                    "buffer_size": int(args.timesteps_per_batch),
+                    "learning_starts": int(50e3),
+                }
+            else:
+                items = {"policy": MLP, "buffer_size": int(args.timesteps_per_batch)}
+        else:
             items = {
                 "policy": MLP,
-                "buffer_size": int(args.timesteps_per_batch),
-                "learning_starts": int(50e3),
+                batchsize: args.timesteps_per_batch,
             }
-        else:
-            items = {"policy": MLP, "buffer_size": int(args.timesteps_per_batch)}
+
+        model = algo(env=vec_env, verbose=2, **items)
+        #model.set_env(env)
+
+        model.learn(total_timesteps=int(args.total_timesteps))
+        model.save("bio_policy")
+        # library helper
+        plot_results(
+            [log_dir],
+            int(args.total_timesteps),
+            results_plotter.X_TIMESTEPS,
+            "TRPO muscle" + identifer,
+        )
+        plt.savefig("convergence_plot" + identifer + ".png")
+        model.save("policy-" + identifer)
+
     else:
-        items = {
-            "policy": MLP,
-            batchsize: args.timesteps_per_batch,
-        }
+        # Use trained policy for the simulation.
+        model = PPO.load("policy-" + identifer)
+        
+        env = make_env(args.SEED*1000)()
+        obs, info_ = env.reset()
 
-    model = algo(env=env, verbose=1, seed=args.SEED, **items)
-    model.set_env(env)
-
-    model.learn(total_timesteps=int(args.total_timesteps))
-    # library helper
-    plot_results(
-        [log_dir],
-        int(args.total_timesteps),
-        results_plotter.X_TIMESTEPS,
-        "TRPO muscle" + identifer,
-    )
-    plt.savefig("convergence_plot" + identifer + ".png")
-    model.save("policy-" + identifer)
-
-else:
-    # Use trained policy for the simulation.
-    model = TRPO.load("trpo_" + identifer)
-    obs = env.reset()
-
-    done = False
-    score = 0
-    while not done:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        score += rewards
-        if info["ctime"] > final_time:
-            break
-    print("Final Score:", score)
-    env.post_processing(
-        filename_video="video-" + identifer + ".mp4", SAVE_DATA=True,
-    )
+        done = False
+        score = 0
+        while not done:
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = vec_env.step(action)
+            score += rewards
+            if info["ctime"] > final_time:
+                break
+        print("Final Score:", score)
+        env.post_processing(
+            filename_video="video-" + identifer + ".mp4", SAVE_DATA=True,
+        )
